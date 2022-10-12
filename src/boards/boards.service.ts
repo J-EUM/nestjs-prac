3cf3422d32591e5149d70c0717fce7cc4105caa6
J-EUM <eumjungmin1@gmail.com>
2022-10-12 17:21:42 +0900

    없는게시물 찾으려할때

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './boards.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -26,7 +26,11 @@ export class BoardsService {
     }
 
     getBoardById(id: string): Board {
-        return this.boards.find((board) => board.id === id);
+        const found = this.boards.find( board => board.id === id);
+        if (!found) {
+            throw new NotFoundException(); // 괄호안에 메세지 넣으면 기본메세지말고 넣은걸로 나옴
+        }
+        return found
     }
 
     deleteBoard(id: string): void {
