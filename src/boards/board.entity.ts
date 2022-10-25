0b846996a68061e961f4822457bd6b060d65b1f2
J-EUM <eumjungmin1@gmail.com>
2022-10-25 21:28:20 +0900

    보드드entity생성

---
+++ src/boards/board.entity.ts
@@ -0,0 +1,17 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BoardStatus } from "./boards.model";
+
+@Entity()
+export class Board extends BaseEntity {
+    @PrimaryGeneratedColumn() // id가 프라이머리키라고 알려주는거
+    id:number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    status: BoardStatus;
+}
